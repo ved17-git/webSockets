7b73b874f7545a039b97cd9de149b04a7d575459
Da4Rkclown <118552956+Da4Rkclown@users.noreply.github.com>
2025-03-09 19:45:01 +0530

    added explanation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,45 @@
 import { useEffect, useState } from "react"
 
-
+//client side
 function App() {
   
   const [text, setText]=useState<string>("");
   const [socket,setSocket]=useState<WebSocket | null >(null)
 
   const handleSend=()=>{
-     
+    //now using that useState hook in const [socket,setSocket]=useState
+    //all the property of ws has come into socket 
+    //sending socket.send, which lets to send the msg from the input box to server
     socket?.send(text)
     setText("")
   }
 
+
+  //on the FIRST MOUNT/RENDER of this component we want to establish a connection, using useEffect
   useEffect(()=>{
    
-    const ws=new WebSocket("ws://localhost:8000")
-    setSocket(ws)
+    const ws=new WebSocket("ws://localhost:8000")  // just like cons res=await axis.get("")
+    //Now we want to use this ws to send to the server using ws.send
+    //but ws is declared in useEffect, hence we create a state-> const [socket,setSocket]=useState() 
+    setSocket(ws) // so that we can use ws
+
+    //here we are getting the response from the server and altering it
+    //(e) => whatever the data from the backend
     ws.onmessage=(e)=>{
       alert(e.data);
     }
+
+    //can give a bunch of callbacks like error,etc.
+
+    // ws.onerror=()=>{
+
+    // }
+    // ws.onopen=()=>{
+
+    // }
+    // ws.onclose()=>{
+
+    // }
 
 
   },[])
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import { WebSocketServer } from "ws";
 
+//creating an instance of websocket
 const wss=new WebSocketServer({port:8000})
 
+// initializing the connection
+//when there is a  "connection" the control reaches inside of the callback 
+//just like app.post('/',(req,res)=>{})
 
+//here it is socket it is persistent, it stays connected 
 wss.on("connection",(socket)=>{
    console.log("Connnected to ws server 8000");
-   
-    socket.on("message",(e)=>{           
+    
+
+   //inside this we can add logic 
+   //socket.on interacting at socket level,  "message" to communicate
+   // the callback (e) gives the message that is coming from the client just like req.body
+    socket.on("message",(e)=>{            
+        //any api logic
         if(e.toString()==="hi"){
-            socket.send("Hello")
+            socket.send("Hello")   //socket.send is used to send the message to the server
         }
         else{
             socket.send("did not said hi, say hi to recieve hello")
